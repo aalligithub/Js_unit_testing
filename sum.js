6a76042c87f4869ca2726c98f883e85d55e6f8c3
aalligithub <ali_kick@yahoo.com>
2022-03-19 23:48:08 +0330

    Adding a dummy function

--- sum.js
+++ sum.js
@@ -2,4 +2,9 @@ function sum(a, b) {
     return a + b
 }
 
+// This function serves no porpuse other than showing changes in coverage
+function balls(){
+    console.log('Balls')
+}
+
 module.exports = sum
